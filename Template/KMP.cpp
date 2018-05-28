52f1be9b83da6b1a2b81a1b956ad0ff6f91e668e
Pritom Kundu <anachored@gmail.com>
2018-05-28 12:23:23 +0600

    Update KMP.cpp

--- Template/KMP.cpp
+++ Template/KMP.cpp
@@ -1,15 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector< int >prefixFunction(const string& s)
+/// everything 1-indexed
+/// v[i] = 0 -> empty string matched
+/// v[i] = k -> prefix s[0..(k-1)] matched
+vector<int> prefixFunction(const string& s)
 {
-    /// everything 1-indexed
-    /// v[i] = 0 -> empty string matched
-    /// v[i] = k -> prefix s[0..(k-1)] matched
-
-    int n = s.size();
-    int k = 0;
-
+    int n = s.size(), k = 0;
+    
     vector< int >v(n+1);
     v[1] = 0;
 
@@ -18,29 +16,23 @@ vector< int >prefixFunction(const string& s)
         if (s[k]==s[i-1]) k++;
         v[i] = k;
     }
-
     return v;
 }
 
 int kmpMatcher(const string& text, const string& pattern)
 {
-    vector< int >pi = prefixFunction(pattern);
-
-    int matchCount = 0;
-    int k = 0;
+    vector<int> pi = prefixFunction(pattern);
+    int matchCount = 0, k = 0;
 
     for (int i = 0; i < text.size(); i++) {
         while (k > 0 && text[i]!=pattern[k]) k = pi[k];
         if (text[i]==pattern[k]) k++;
 
-        if (k==pattern.size()) {
-            /// full pattern match found
-
+        if (k==pattern.size()) {    /// full pattern match found
             matchCount++;
             k = pi[k];
         }
     }
-
     return matchCount;
 }
 
